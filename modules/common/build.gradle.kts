cfff01df96759175b52deff7199f05c7de1214f9
Tetiana Diachuk <diacht@gmail.com>
2023-05-02 23:46:07 +0300

    Update build.gradle.kts

--- modules/common/build.gradle.kts
+++ modules/common/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:Versions.coroutines")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}")
 }
 
 tasks.test {
