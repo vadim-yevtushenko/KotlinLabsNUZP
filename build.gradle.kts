b953b3ae7dcf6c848eda17d83b91e86c03c3c08c
Vadim Yevtushenko <vadik.chicoto@gmail.com>
2025-11-24 09:56:22 +0200

    lab_4

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ subprojects {
     apply(plugin = "com.github.gmazzo.buildconfig")
 }
 
-val labNumber = 3
+val labNumber = 4
 
 allprojects {
     buildConfig {
--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -1,17 +1,47 @@
-import com.diacht.ktest.FactoryItf
 import com.diacht.ktest.compose.startTestUi
+import kotlinx.coroutines.*
 import org.example.helloworld.BuildConfig
+import java.net.URL
+import kotlin.math.sqrt
 
 fun seed(): String = "vadim-yevtushenko"
 
-fun labNumber() : Int = BuildConfig.LAB_NUMBER
+fun labNumber(): Int = BuildConfig.LAB_NUMBER
 
-fun getSimulationObject(): FactoryItf {
-    return CoffeeShop()
+suspend fun serverDataCalculate(strList: List<String>): Double {
+    if (strList.size < 6) {
+        throw RuntimeException("Expected list size: 6")
+    }
+    val numbers = fetchDataFromServer(strList)
+    val mult = numbers.fold(1.0){ acc, value ->
+        acc * value
+    }
+
+    return sqrt(mult)
 }
 
-    fun main(args: Array<String>) {
-        println("Лабораторна робота №${labNumber()} користувача ${seed()}")
+suspend fun getNumberFromServer(message: String): Int {
+    return withContext(Dispatchers.IO) {
+        val url = URL("http://diacht.2vsoft.com/api/send-number?message=$message")
+        val connection = url.openConnection()
+        connection.connect()
+        val input = connection.getInputStream()
+        val buffer = ByteArray(128)
+        val bytesRead = input.read(buffer)
+        input.close()
+        String(buffer, 0, bytesRead).toInt()
+    }
+}
+
+suspend fun fetchDataFromServer(strList: List<String>): List<Int> = coroutineScope {
+    val deferredList = strList.map {
+        async { getNumberFromServer(it) }
+    }
+    return@coroutineScope deferredList.awaitAll()
+}
+
+fun main(args: Array<String>) {
+    println("Лабораторна робота №${labNumber()} користувача ${seed()}")
         startTestUi(seed(), labNumber())
 
-    }
+}
